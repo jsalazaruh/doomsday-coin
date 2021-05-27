1a167cee3108b2447dae542e8049eadc67072fc1
jsalazaruh <jonathansalazar94@gmail.com>
2021-05-27 15:21:22 -0500

    changing particles

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -124,7 +124,7 @@ const Header = ({
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Buy now</Link>
                       </li>
                     </ul>}
                 </div>
